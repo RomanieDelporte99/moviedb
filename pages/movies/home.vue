ccb3e0d0803d6d607fe90666027dddfc14133d4d
Romanie Delporte <romanie.delporte@justrussel.com>
2025-05-27 20:15:09 +0200

    some changes

--- pages/movies/home.vue
+++ pages/movies/home.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
     <MovieCard
-        v-for="(movie, index) in movies" :key="index"
-        class="border border-neutral-4OO rounded-xl shadow-md w-full cursor-pointer"
-        @click="navigateTo(`/movies/${movie.id}`)"
+      v-for="(movie, index) in movies"
+      :key="index"
+      class="border border-neutral-4OO rounded-xl shadow-md w-full cursor-pointer"
+      @click="navigateTo(`/movies/${movie.id}`)"
     >
       <template #header>
         <img
-            :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" alt="Movie Poster"
-            class="rounded-t-xl h-full w-full object-cover"
+          :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
+          alt="Movie Poster"
+          class="rounded-t-xl h-full w-full object-cover"
         >
       </template>
 
@@ -16,29 +18,28 @@
         <div class="flex justify-between">
           <h2 class="text-lg font-bold max-w-52">{{ movie.title }}</h2>
           <div>
-            <MovieTag severity="secondary" :value="movie.vote_average.toFixed(1)"/>
+            <MovieTag
+              severity="secondary"
+              :value="movie.vote_average.toFixed(1)"
+            />
           </div>
         </div>
       </template>
-
-      <template #footer>
-      </template>
     </MovieCard>
-
   </div>
 </template>
 <script setup>
 definePageMeta({
   layout: 'layout'
-})
+});
 const moviesStore = useMovieStore();
 
 onMounted(async () => {
   await moviesStore.fetchMovies();
-})
+});
 
 const movies = computed(() => {
   return moviesStore.movies;
-})
+});
 </script>
 
