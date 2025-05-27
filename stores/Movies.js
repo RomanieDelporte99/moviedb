54a5f704f522ba0b9d02fb5ae94875710cf79805
Romanie Delporte <romanie.delporte@justrussel.com>
2025-05-27 20:16:54 +0200

    udpate formatting

--- stores/Movies.js
+++ stores/Movies.js
@@ -1,45 +1,45 @@
 import { defineStore } from 'pinia';
 
 export const useMovieStore = defineStore('movie', {
-    state: () => {
-        return {
-            movies: [],
-            movieDetail: {},
-            popularMovies: []
-        }
+  state: () => {
+    return {
+      movies: [],
+      movieDetail: {},
+      popularMovies: []
+    };
+  },
+
+  actions: {
+    async fetchPopularMovies() {
+      try {
+        const { $movieDBApi } = useNuxtApp();
+        $movieDBApi('popular?language=en-US&page=1')
+          .then(data => this.movies = data.results)
+          .catch(err => console.error(err));
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
     },
+    async fetchMovies() {
+      try {
+        const { $movieDBApi } = useNuxtApp();
 
-    actions: {
-        async fetchPopularMovies() {
-            try {
-                const { $movieDBApi } = useNuxtApp()
-                $movieDBApi('popular?language=en-US&page=1')
-                    .then(data => this.movies = data.results)
-                    .catch(err => console.error(err));
-            } catch (error) {
-                console.error('Error fetching movies:', error);
-            }
-        },
-        async fetchMovies() {
-            try {
-                const { $movieDBApi } = useNuxtApp()
-
-                $movieDBApi('now_playing?language=en-US&page=1')
-                    .then(data => this.movies = data.results)
-                    .catch(err => console.error(err));
-            } catch (error) {
-                console.error('Error fetching movies:', error);
-            }
-        },
-        async fetchMovieDetails(movieId) {
-            try {
-                const { $movieDBApi } = useNuxtApp()
-                $movieDBApi(`${movieId}?language=en-US`)
-                    .then(data => this.movieDetail = data)
-                    .catch(err => console.error(err));
-            } catch (error) {
-                console.error('Error fetching movie details:', error);
-            }
-        }
+        $movieDBApi('now_playing?language=en-US&page=1')
+          .then(data => this.movies = data.results)
+          .catch(err => console.error(err));
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    },
+    async fetchMovieDetails(movieId) {
+      try {
+        const { $movieDBApi } = useNuxtApp();
+        $movieDBApi(`${movieId}?language=en-US`)
+          .then(data => this.movieDetail = data)
+          .catch(err => console.error(err));
+      } catch (error) {
+        console.error('Error fetching movie details:', error);
+      }
     }
-})
+  }
+});
